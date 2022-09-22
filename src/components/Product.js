7b679ccabc4dbe41f0d660adcb7da53731d5b249
ALMA_DEV914 <aungonalna58@gamil.com>
2022-09-22 01:14:20 -0700

    create function for buttons

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,40 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/product.scss';
+import React, { useEffect, useState } from "react";
+import "../styles/product.scss";
 
 function Product() {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [active, setActive] = useState("")
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [active, setActive] = useState("");
 
-useEffect(() => {
-    fetch('https://fakestoreapi.com/products?limit=5')
-    .then((response) => {
-    if (!response.ok) {
-        throw new Error(
-          `This is an HTTP error: The status is ${response.status}`
-        );
-      }
-      return response.json();
-    })
-    .then((actualData) => {
-        setData(actualData)
-        console.log(actualData)
-        setError(null)
-   })
-    .catch((err) => {
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=5")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        console.log(actualData);
+        setError(null);
+      })
+      .catch((err) => {
         setError(err.message);
-        setData(null)
-    })
-    .finally(() => {
-        setLoading(false)
-    })
-}, []);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-const handleClick = (event) => {
-   setActive(event.target.id);
-  
-}
+  const handleClick = (event) => {
+    setActive(event.target.id);
+  };
+
+  const ModalDescription = (data) => {
+    return (
+     
+    <div className="product-left-details" id={`${data.id}`}>
+    <p className="product-price">
+      <b>Price: ${data.price}</b>
+    </p>
+    <p>{data.description}</p>
+    <p>
+      {data.rating.rate} {data.rating.count}
+    </p>
+    <button className="addCartBtn">Add to cart</button>
+  </div>
+       
+    )
+  }
 
   return (
     <div>
@@ -46,25 +62,41 @@ const handleClick = (event) => {
         {data &&
           data.map((data, index) => (
             <div key={index} className="product-list">
-              <div className='product-right-details'>
-               <a href={data.id}><img src={data.image} alt="product"/></a>
-               <a href={data.id} className='product-title'> <h3>{data.title}</h3></a>
-    
-               <i onClick={handleClick} key={index} id={"0"}  className={active === "0" ? 'fa fa-chevron-right active'
-            : "fa fa-chevron-left"}></i>
-                </div>
-             <div className='product-left-details'>
-              <p className='product-price'><b>Price: ${data.price}</b></p>
-              <p>{data.description}</p>
-               <p>{data.rating.rate} {data.rating.count}</p>
-            <button className='addCartBtn'>Add to cart</button>
-            </div>
-            </div>
+              <div className="product-right-details">
+                <a href={data.id}>
+                  <img src={data.image} alt="product" />
+                </a>
+                <a href={data.id} className="product-title">
+                  {" "}
+                  <h3>{data.title}</h3>
+                </a>
+
+                <i
+                  onClick={handleClick}
+                  key={index}
+                  id={`${data.id}`}
+                  className={
+                    active === `${data.id}`
+                      ? "fa fa-chevron-right active"
+                      : "fa fa-chevron-left"
+                  }
+                ></i>
+              </div>
+              <div className="product-left-details" id={`${data.id}`}>
+    <p className="product-price">
+      <b>Price: ${data.price}</b>
+    </p>
+    <p>{data.description}</p>
+    <p>
+      {data.rating.rate} {data.rating.count}
+    </p>
+    <button className="addCartBtn">Add to cart</button>
+  </div>
+          </div>
           ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default Product
-
+export default Product;
